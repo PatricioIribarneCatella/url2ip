1c55f2958affc7109d0a768f77573a9ca222d68a
PatricioIribarneCatella <patricio@patricio-Lenovo-V360>
2018-02-01 15:23:50 -0300

    Some printing changes on the data

--- main.py
+++ main.py
@@ -7,14 +7,35 @@ def get_data():
     print "Type the host name and press 'Enter'\n"
     print "     (For example: google.com)\n"
 
-    return str(raw_input())
+    h = str(raw_input())
+    print "\n"
+
+    return h
+
+def print_data(hn, al, ipl):
+
+    print "- Host name: " + hn + "\n"
+
+    print "- Alias list"
+    print "     (alternative host names, possibly empty)"
+    if (len(al)):
+        for a in al:
+            print a + "\n"
+    else:
+        print " ---- \n"
+
+    print "- Addresses for the same interface on the same host"
+    print "     (often but not always a single address)"
+    for ip in ipl:
+        print ip + "\n"
 
 def url2ip():
 
-    hostName = get_data()
-    ips = s.gethostbyname_ex(hostName)
-    
-    print ips
+    hn = get_data()
+
+    (hostName, aliasList, ipAddrList) = s.gethostbyname_ex(hn)
+
+    print_data(hostName, aliasList, ipAddrList)
 
 if __name__ == '__main__':
     url2ip()
